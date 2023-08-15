4077cf812bdce07217a1447dc77d961eb549351c
sugarkwork <sugarkwork@sugar-knight.com>
2023-08-16 01:15:50 +0900

    Delete superfluous comments.

--- webui.py
+++ webui.py
@@ -3,19 +3,3 @@ from mozaikukun_ui import full_ui
 if __name__=="__main__":
     demo = full_ui()
     demo.launch()
-
-"""
-demo = gr.Interface(
-    fn=mosaic_process,
-    inputs=[
-        gr.Image(),
-        gr.Radio(choices=['raw', 'mosaic', 'white'], value='mosaic'),
-        gr.Radio(choices=['raw', 'mosaic', 'white'], value='mosaic'),
-        gr.Radio(choices=['raw', 'mosaic', 'white'], value='mosaic'),
-        gr.Radio(choices=['raw', 'mosaic', 'white'], value='raw'),
-        gr.Radio(choices=['raw', 'mosaic', 'white'], value='raw'),
-    ],
-    outputs=["image"])
-
-demo.launch()
-"""
