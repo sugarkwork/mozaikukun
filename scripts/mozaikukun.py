196f537984725895dda27932987bb4b51e932eaa
ai-tech-girl <131214707+ai-tech-girl@users.noreply.github.com>
2023-08-16 10:11:37 +0900

    Option to save original image in sd

--- scripts/mozaikukun.py
+++ scripts/mozaikukun.py
@@ -1,7 +1,9 @@
 import gradio as gr
 
 import modules.scripts as scripts
-from modules import script_callbacks
+from modules import script_callbacks, images
+from modules.shared import opts
+from modules.processing import create_infotext
 
 from mozaikukun_ui import full_ui, inputs_ui, mosaic_process
 
@@ -21,16 +23,27 @@ class Mozaikukun(scripts.Script):
                     value=False,
                     visible=True,
                 )
-                elements = [enabled]
+                save_original = gr.Checkbox(
+                    label="Save original image",
+                    value=False,
+                    visible=True,
+                )
+                elements = [enabled, save_original]
             with gr.Row():
                 elements.extend(inputs_ui())
         return elements
 
     def postprocess_image(
-            self, p, pp, enabled, *args):
+            self, p, pp, enabled, save_original, *args):
 
         if not enabled:
             return
+
+        def infotext(index=0, use_main_prompt=False):
+            return create_infotext(p, p.prompts, p.seeds, p.subseeds, use_main_prompt=use_main_prompt, index=index, all_negative_prompts=p.negative_prompts)
+
+        if save_original:
+            images.save_image(pp.image, p.outpath_samples, "", p.seeds[p.batch_index], p.prompts[p.batch_index], opts.samples_format, info=infotext(p.batch_index), p=p)
 
         pp.image = mosaic_process(pp.image, *args)
 
